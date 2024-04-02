afb8e559f9f3716917719a1ef4c7bd5bc368528b
Jens Reimann <ctron@dentrassi.de>
2024-04-02 12:02:51 +0200

    chore: convert bullseye example to function component example

--- src/layouts/bullseye/mod.rs
+++ src/layouts/bullseye/mod.rs
@@ -2,25 +2,15 @@ use crate::{example, example::ExamplePage};
 use patternfly_yew::prelude::*;
 use yew::prelude::*;
 
-pub struct BullseyeExample {}
+#[function_component(BullseyeExample)]
+pub fn bullseye_example() -> Html {
+    let example1 = example! ("Bullseye" => "bullseye.1.example");
 
-impl Component for BullseyeExample {
-    type Message = ();
-    type Properties = ();
-
-    fn create(_: &Context<Self>) -> Self {
-        Self {}
-    }
-
-    fn view(&self, _: &Context<Self>) -> Html {
-        let example1 = example! ("Bullseye" => "bullseye.1.example");
-
-        html! {
-            <>
-                <ExamplePage title="Bullseye Layout">
-                    {example1}
-                </ExamplePage>
-            </>
-        }
+    html! {
+        <>
+            <ExamplePage title="Bullseye Layout">
+                {example1}
+            </ExamplePage>
+        </>
     }
 }
